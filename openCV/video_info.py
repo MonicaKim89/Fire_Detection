38326e7c59f7795918ecec5c299e00d971faa025
Monica Kim <monicakim89@naver.com>
2021-05-14 15:16:48 +0900

    video_info.py added

---
+++ openCV/video_info.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import cv2
+import cv2 as cv
+from FINAL_IMG_PREP import *
+
+
+# In[1]:
+
+
+def vid_info(path, codec, name):
+    cap = cv2.VideoCapture(path)
+    print(cap)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    #재생할 파일의 높이 얻기
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    #재생할 파일의 프레임 레이트 얻기
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    #codec
+    fourcc = cv2.VideoWriter_fourcc(*codec)
+    
+    #filename
+    filename = name+'.mp4'
+    
+    #out 
+    out = cv2.VideoWriter(filename, fourcc, fps, (int(width), int(height)))
+    
+    print('cap {0}, width {1}, height {2}, fps {3}'.format(cap, width, height, fps))
+    print('codec {0}', 'filename {1}'.format(fourcc, filename))
+    
+    return cap, width, height, fps, fourcc, filename, out
+
+
+# In[2]:
+
+
+def background(cap):
+    ret, back = cap.read()
+    if not ret:
+        print('Image registration failure')
+        
+    back = cv2.cvtColor(back, cv2.COLOR_BGR2GRAY)
+    back = cv2.GaussianBlur(back, (0,0),3)
+    return back
+
+
+# In[ ]:
+
+
+
+
